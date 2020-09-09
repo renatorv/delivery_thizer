2764805bb668157382be2469807dbcd6fe2dedbc
renatorv <renatorv@gmail.com>
2020-09-08 23:43:17 -0300

    inicial

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:delivery_thizer/login_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      enableLog: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: GoogleFonts.cantarellTextTheme(Theme.of(context).textTheme),
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+//figma
+//dribble
+// gabul dev
